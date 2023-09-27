249bd15bf3c5f63fb6356012c9c01025595e999e
RafaWill23 <141331069+RafaWill23@users.noreply.github.com>
2023-09-27 12:31:14 -0400

    Add files via upload

---
+++ src/componentes/footer/Footer.tsx
@@ -0,0 +1,25 @@
+import "./Footer.css";
+function Footer(){
+    return(
+        <footer>
+            <section>
+                
+
+<img src="https://static.vecteezy.com/system/resources/previews/016/716/469/original/instagram-icon-free-png.png" alt="" />
+<img src="https://cdn.icon-icons.com/icons2/1476/PNG/512/twitter_101809.png" alt="" />
+<img src="https://cdn.icon-icons.com/icons2/91/PNG/512/facebook_16423.png" alt="" />
+
+            </section>
+
+            <section>
+            <a href="#">Termos de Serviço</a>
+           <a href="#"> Política de Privacidade</a>
+           </section>
+           <section>
+            <p className="texto-footer">@Rafa_Will</p>
+           </section>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/componentes/main/Main.tsx
@@ -0,0 +1,79 @@
+import {useState} from 'react'
+import Projeto from '../projeto/Projeto'
+import "./Main.css";
+
+type ProjetoType={
+    id:number,
+    titulo:string,
+    descricao:string,
+    imagem:string
+}
+export default function Main(){
+    const [texto,setTexto]=useState("")
+    const projetos:ProjetoType[] = [
+        {
+            id:1,
+            titulo:'Ritmos da vida',
+            descricao:"É um Projeto no qual eu participo no IFMaker juntamente com outros estudantes e o professor Márcio, onde comentamos sobre a vida e suas adversidades",
+            imagem:'/ritmosdavida.jpg'  
+        },
+        {
+            id:2,
+            titulo:'PCMaker',
+            descricao:'Site criado para mostrar os melhores custo-benefícios de peças para computadores gamers',
+            imagem:'/pcmaker.png'
+        },
+        {
+            id:3,
+            titulo:'Visita Técnica ao parque estadual várzeas do rio ivinhema',
+            descricao:'Chegamos lá umas 10:00 e conhecemos o lugar, fizemos trilhas, plantamos árvores, conhecemos sobre o parque e os animais que lá habitam.',
+            imagem:'/rio.jpg'
+        },
+        {
+            id:4,
+            titulo:'Site da Barbie',
+            descricao:"Site que fizemos em aula e mostra os filmes com pesquisa.",
+            imagem:'/barbie.png'
+        },
+        {
+            id:5,
+            titulo:'Visita Técnica à Foz do Iguaçu',
+            descricao:'Viajamos por 7 horas, ficamos em um hotel, conhecemos a Itaipu(que foi o principal), as Cataratas, o templo budista e o shopping, mas a viagem foi incrível.',
+            imagem:'/foz.jpg'
+        },
+    ]
+    function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+        setTexto(e.target.value)
+    }
+    return(
+        <>
+        <div className="campo_pesquisa">
+            <p>Busque:</p>
+            <input type="text" 
+            className='botao_pesquisa'
+            placeholder='Pesquise um Projeto'
+            onChange={TrataTexto}/>
+            
+        </div>
+        <div>
+        {texto && <p className='resultados'>Resultados para: {texto}</p>}
+        </div>
+        <main className="content-main">
+            {
+                projetos.filter((projeto)=>projeto.titulo.toLowerCase().includes(texto)).map(
+                    (projeto)=>
+                    <Projeto
+                    key={projeto.id}
+                    descricao={projeto.descricao}
+                    titulo={projeto.titulo}
+                    imagem={projeto.imagem}
+                    />
+                )
+            }
+        </main>
+        </>
+    )
+}
+
+
+
